f8864c6601ca44729badf2c2846e51e49cfb1bdb
Joakim VL <joakim.van.laethem@student.howest.be>
2019-11-22 14:19:53 +0100

    add constants to WebAPI

---
+++ Oefenplatform.WebAPI/Constants/QuestionCategories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oefenplatform.WebAPI.Constants
+{
+    public class QuestionCategories
+    {
+        public const string LangQuestionFirstGrade = "FotoSelectie";
+        public const string MathQuestionFirstGrade = "Splitsen";
+        public const string LangQuestionSecondGrade = "Lezen";
+        public const string MathQuestionSecondGrade = "Tafels";
+        public const string LangQuestionThirdGrade = "Klanken";
+    }
+}
