3247f0772a1ba0c27c8a554570a526a9ab0e5324
Sjoerd Heuvelman <Sjoerd_Heuvelman@hotmail.com>
2019-12-27 16:02:21 +0100

    Extend LangFirstGradeQuestionDto

--- Oefenplatform.Lib/DTO/QuestionDto/LangFirstGradeQuestionDto.cs
+++ Oefenplatform.Lib/DTO/QuestionDto/LangFirstGradeQuestionDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oefenplatform.Lib.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,11 @@ namespace Oefenplatform.Lib.DTO.QuestionDto
 {
     public class LangFirstGradeQuestionDto
     {
+        public int Id { get; set; }
         public string QuestionTitle { get; set; }
         public string FileName { get; set; }
         public string Answer { get; set; }
+        public ICollection<Feedback> Feedback { get; set; }
+        public int QuestionCategory { get; set; } = 2;
     }
 }
