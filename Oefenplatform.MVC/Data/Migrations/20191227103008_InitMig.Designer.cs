7030301b402dafea1b86c015271df091e63eecfd
Wout Gheysen <Projecthexd@gmail.com>
2019-12-31 10:15:52 +0100

    Add data migrations

--- Oefenplatform.MVC/Data/Migrations/20191227103008_InitMig.Designer.cs
+++ Oefenplatform.MVC/Data/Migrations/20191227103008_InitMig.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oefenplatform.MVC.Data;
 
 namespace Oefenplatform.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191227103008_InitMig")]
+    partial class InitMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
