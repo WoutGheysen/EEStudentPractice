d3b1572e0375377d0c09839f19518f42f4586e62
Sjoerd Heuvelman <Sjoerd_Heuvelman@hotmail.com>
2020-01-04 10:20:41 +0100

    Add ViewComponent (Disabled)

---
+++ Oefenplatform.MVC/Models/Shared/NavbarVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oefenplatform.MVC.Models.Shared
+{
+    public class NavbarVm
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Area { get; set; }
+    }
+}
