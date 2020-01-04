13bccefad412b0cf1d4a42ac723e0517f3dbe8e9
Sjoerd Heuvelman <Sjoerd_Heuvelman@hotmail.com>
2020-01-04 14:15:58 +0100

    Add Layout to AssessmentFirstLang

--- Oefenplatform.MVC/Areas/Teacher/Models/AssessmentFirstLang/AssessmentFirstLangIndexVm.cs
+++ Oefenplatform.MVC/Areas/Teacher/Models/AssessmentFirstLang/AssessmentFirstLangIndexVm.cs
@@ -1,6 +1,7 @@
 ﻿using Oefenplatform.Lib.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Oefenplatform.MVC.Areas.Teacher.Models.AssessmentFirstLang
 {
     public class AssessmentFirstLangIndexVm
     {
+        [Display(Name = "Testen")]
         public ICollection<Assessment> SelectedAssessments { get; set; }
     }
 }
