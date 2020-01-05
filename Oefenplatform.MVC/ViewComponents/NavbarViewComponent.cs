83b60f624c075901dcac41556eb3693f5109b588
Sjoerd Heuvelman <Sjoerd_Heuvelman@hotmail.com>
2020-01-05 12:16:28 +0100

    Add Nullcheck to NavbarViewComponent

--- Oefenplatform.MVC/ViewComponents/NavbarViewComponent.cs
+++ Oefenplatform.MVC/ViewComponents/NavbarViewComponent.cs
@@ -28,10 +28,19 @@ namespace Oefenplatform.MVC.ViewComponents
             var userIdentity = User as ClaimsPrincipal;
 
             string loggedUserId = _user.GetUserId(userIdentity);
-
+            if(loggedUserId == null)
+            {
+                var viewModel = new NavbarVm()
+                {
+                    Action = "Index",
+                    Controller = "Home"
+                };
+                return await Task.FromResult<IViewComponentResult>(View(viewModel));
+            }
             string userByIdentityReference = $"{fullLink}/IdRef/{loggedUserId}";
             var user = WebApiService.GetApiResult<SchoolUser>(userByIdentityReference);
             ViewBag.Mode = user.SchoolUserCategory.Category;
+
             if (user.SchoolUserCategory.Category == "Teacher")
             {
                 var viewModel = new NavbarVm()
